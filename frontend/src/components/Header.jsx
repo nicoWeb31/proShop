82e5fb4b885859329df6b42379b11b1bbf192848
nicoWeb31 <nico.riot@free.fr>
2020-12-08 16:08:56 +0100

    logout action and udate header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import {useDispatch, useSelector} from 'react-redux';
+import { Navbar, Nav, Container,NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+//action
+import { logout } from '../actions/userAction'
+
 
 const Header = () => {
+
+    const userLogin = useSelector(state =>state.userLogin);
+    const {userInfo} = userLogin;
+
+    const dispatch = useDispatch();
+
+
+    //______________________________function____________________________
+    const logoutHandler =()=>{
+        //logoutHandler action
+        dispatch(logout());
+    }
+
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
@@ -24,6 +41,15 @@ const Header = () => {
                                 </Nav.Link>
                             </LinkContainer>
 
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) :
+                            (
                             <LinkContainer to="/login">
                                 <Nav.Link>
                                     <i
@@ -33,6 +59,9 @@ const Header = () => {
                                     Sign In
                                 </Nav.Link>
                             </LinkContainer>
+
+                            )}
+
                         </Nav>
                     </Navbar.Collapse>
                 </Container>

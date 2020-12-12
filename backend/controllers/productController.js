05077d4def9963b9c1ba207a3d77b964113b5a5d
nicoWeb31 <nico.riot@free.fr>
2020-12-12 17:58:00 +0100

    back createProduct and edit it

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,6 @@ const test = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'ok' });
 });
 
-
 //@desc Fetch all products
 //@route Get /api/products
 //@access Public
@@ -18,7 +17,6 @@ const fetchAllPoducts = asyncHandler(async (req, res) => {
         products,
     });
 });
-
 
 //@desc Fetch single product
 //@route Get /api/produccts/id
@@ -33,28 +31,109 @@ const fetchOneProduct = asyncHandler(async (req, res) => {
         });
     } else {
         //status code diff de 200 custom error handler
-        res.status(404)
-        throw new Error("product not found");
+        res.status(404);
+        throw new Error('product not found');
     }
 });
-
 
 //@desc delete for admin
 //@route delete /api/products/:id
 //@access Privé/Admin
 //_____________________fetch by ID_______________________________________________
 const deleteProduct = asyncHandler(async (req, res) => {
-
     const product = await Product.findByIdAndDelete(req.params.id);
 
-    if(!product) {
-        throw new Error('product not found!!!')
+    if (!product) {
+        throw new Error('product not found!!!');
     }
 
     res.status(204).json({
-        status: "success",
+        status: 'success',
         data: null,
     });
+});
+
+//@desc create for admin
+//@route POST /api/products
+//@access Privé/Admin
+//_____________________create a new product_______________________________________________
+const createProduct = asyncHandler(async (req, res) => {
+    // const product = await Product.create(req.body)
+
+    // res.status(201).json({
+    //     status: "success",
+    //     product
+    // });
+
+    const product = new Product({
+        name: 'Sample Product',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample Brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
+    });
+
+    const createProduct = await product.save();
+    console.log("🚀 ~ file: productController.js ~ line 81 ~ createProduct ~ createProduct", createProduct)
+    res.status(201).json({
+        status: 'success',
+        product: createProduct,
+    });
+});
+
+//@desc edit for admin
+//@route PUT /api/products/:id
+//@access Privé/Admin
+//_____________________edit by ID_______________________________________________
+const editProduct = asyncHandler(async (req, res) => {
+    // const {
+    //     name,
+    //     price,
+    //     description,
+    //     image,
+    //     category,
+    //     brand,
+    //     countInStock,
+    // } = req.body;
+
+    // const product = await Product.findById(req.params.id);
+    // if (product) {
+    //     product.name = name;
+    //     product.price = price;
+    //     product.description = description;
+    //     product.image = image;
+    //     product.category = category;
+    //     product.brand = brand;
+    //     product.countInStock = countInStock;
+
+    //     const updatedProduct = await product.save({runValidators :true});
+
+    //     res.status(201).json({
+    //         status: 'success',
+    //         product: updatedProduct,
+    //     });
+    // } else {
+    //     throw new Error('Product not found');
+    // }
+
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body,{
+        new: true,
+        runValidators:true
+    });
+
+    if(!product){
+        throw new Error('Product not found');
+    }else{
+        res.status(201).json({
+            status: 'success',
+            product,
+        })
+    }
+
 
 });
 
@@ -62,5 +141,7 @@ export {
     fetchOneProduct,
     fetchAllPoducts,
     test,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    editProduct,
 };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,16 +2,24 @@ import express from 'express';
 import {
     fetchAllPoducts,
     fetchOneProduct,
-    deleteProduct
+    deleteProduct,
+    editProduct,
+    createProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddlware.js';
 
 const router = express.Router();
 
-//@desc Fetch all products
-//@route Get /api/products
-//@access Public
-router.route('/').get(fetchAllPoducts);
+router
+    .route('/')
+    //@desc Fetch all products
+    //@route Get /api/products
+    //@access Public
+    .get(fetchAllPoducts)
+    //@desc create for admin
+    //@route POST /api/products
+    //@access Privé/Admin
+    .post(protect, admin, createProduct);
 
 router
     .route('/:id')
@@ -22,6 +30,10 @@ router
     //@desc delete for admin
     //@route delete /api/products/:id
     //@access Privé/Admin
-    .delete(protect, admin, deleteProduct);
+    .delete(protect, admin, deleteProduct)
+    //@desc edit for admin
+    //@route PATCH /api/products/:id
+    //@access Privé/Admin
+    .patch(protect, admin, editProduct);
 
 export default router;
